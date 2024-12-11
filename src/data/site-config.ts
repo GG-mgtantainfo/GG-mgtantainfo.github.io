95fad0ebd3aefa25262b392c9f90a62eec1433c7
GG-mgtantainfo <mgtantainfo@gmail.com>
2024-12-11 20:10:25 +0000

    update site info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,8 +38,8 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: 'Me agobia tanta info',
+    subtitle: 'Glitter Glue ✨',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -59,8 +59,8 @@ const siteConfig: SiteConfig = {
             href: '/blog'
         },
         {
-            text: 'Tags',
-            href: '/tags'
+            text: 'Github',
+            href: 'https://github.com/GG-mgtantainfo'
         }
     ],
     footerNavLinks: [
@@ -76,10 +76,6 @@ const siteConfig: SiteConfig = {
             text: 'Terms',
             href: '/terms'
         },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
     ],
     socialLinks: [
         {
